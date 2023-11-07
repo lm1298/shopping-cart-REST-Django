9907acbbd365c3d42158bee027d007352fb07b8c
Lakshmi Prasanna Malempati <lm1298@usnh.edu>
2023-11-06 23:35:03 -0500

    create serializers for shopping api

---
+++ cart/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Product
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'instagram_handle')
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
