df5fd51814290ed095a9b019cdd50a99c7fd1c7a
Lakshmi Prasanna Malempati <lm1298@usnh.edu>
2024-02-27 10:55:07 -0500

    update product models

--- cart/models.py
+++ cart/models.py
@@ -5,33 +5,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Product(models.Model):
-    """
-    A class representing a product in the online store.
-
-    Fields:
-    - name: The name of the product.
-    - description: A description of the product (optional).
-    - price: The price of the product.
-    - image: An image representing the product (optional).
-    - is_available: A flag indicating whether the product is currently available.
-    - created_at: The timestamp when the product was created.
-    - modified_at: The timestamp when the product was last modified.
-    - id: A unique identifier for the product.
-
-    Methods:
-    - __str__: Get a string representation of the product.
-
-    Reference:
-    https://dev.to/nick_langat/building-a-shopping-cart-using-django-rest-framework-54i0
-    """
-    name = models.CharField(max_length=150)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    is_available = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-    id = models.BigAutoField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255, default='none')
+    price = models.CharField(max_length=100) 
+    category = models.CharField(max_length=255, default='Uncategorized')
+    description = models.TextField()
+    image = models.URLField()
 
     def __str__(self) -> str:
         """
