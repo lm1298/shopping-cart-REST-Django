3c0aea699f69cb19adce27572a05a5f29249636d
Lakshmi Prasanna Malempati <lm1298@usnh.edu>
2024-03-04 23:03:24 -0500

    modify cart and product models

--- cart/models.py
+++ cart/models.py
@@ -4,7 +4,10 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import User
 
+from shopping import settings
+
 class Product(models.Model):
+    name = models.CharField(max_length=255, default='Default Name')
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=255, default='none')
     price = models.CharField(max_length=100) 
@@ -20,3 +23,13 @@ class Product(models.Model):
             str: String representation of the product.
         """
         return str(self.name)
+
+class Cart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
+    objects = models.Manager()
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    quantity = models.IntegerField(default=1)
+
